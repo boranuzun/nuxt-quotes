a89753681c2a3959dfa58740546f04280eb967a6
Boran <boran.u3@gmail.com>
2023-07-03 16:37:29 +0200

    Updated 'New Quote' button

--- components/Quotes.vue
+++ components/Quotes.vue
@@ -63,7 +63,7 @@ fetchQuote();
       </blockquote>
       <figcaption class="flex items-center justify-center mt-6 space-x-3">
         <div class="flex items-center divide-x-2 divide-gray-500">
-          <cite class="pr-3 font-medium text-gray-900">
+          <cite class="font-medium text-gray-900">
             — {{ quote?.author }} —
           </cite>
         </div>
@@ -74,10 +74,23 @@ fetchQuote();
     <div class="flex items-center justify-center flex-col">
       <button
         @click="fetchQuote"
-        class="my-16 px-6 py-4 bg-gray-900 text-white rounded-md"
+        class="my-16 p-4 bg-gray-900 text-white rounded-md inline-flex items-center justify-center space-x-2 hover:bg-gray-800 shadow-md hover:shadow-2xl group"
       >
-        New Quote
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          class="w-6 h-6 transition-transform duration-300 transform group-hover:rotate-180"
+        >
+          <path
+            fill="currentColor"
+            d="M17.65 6.35a7.95 7.95 0 0 0-6.48-2.31c-3.67.37-6.69 3.35-7.1 7.02C3.52 15.91 7.27 20 12 20a7.98 7.98 0 0 0 7.21-4.56c.32-.67-.16-1.44-.9-1.44c-.37 0-.72.2-.88.53a5.994 5.994 0 0 1-6.8 3.31c-2.22-.49-4.01-2.3-4.48-4.52A6.002 6.002 0 0 1 12 6c1.66 0 3.14.69 4.22 1.78l-1.51 1.51c-.63.63-.19 1.71.7 1.71H19c.55 0 1-.45 1-1V6.41c0-.89-1.08-1.34-1.71-.71l-.64.65z"
+          />
+        </svg>
+        <span>New Quote</span>
       </button>
+
       <footer>
         <div class="p-1 text-sm italic">
           <span>Built with </span
